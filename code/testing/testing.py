33e70963c9ae3624d8eb90715552e719473623d5
jworboys <Jordworb@gmail.com>
2021-04-07 22:13:21 -0400

    Adding Testing

--- code/testing/testing.py
+++ code/testing/testing.py
@@ -4,7 +4,6 @@ import requests
 
 
 # --- POST TESTING ---
-
 # Testing obtaining a post.
 def test_get_existing_post_by_id():
     response = requests.get("http://127.0.0.1:5000/post/2")
@@ -31,7 +30,6 @@ def test_creating_post_with_false_data():
 
 
 # --- REPLY TESTING ---
-
 # Reply to post
 def test_create_a_reply():
     myobj = {"content": "Testing Reply", "post_id": 2, "user_id": 1}
@@ -55,6 +53,11 @@ def test_liking_a_post():
     assert response.status_code == 201
 
 
+
+
+
+
+# -- REMOVE TESTING ---
 # Removing like
 def test_removing_a_like():
     response = requests.delete("http://127.0.0.1:5000/remove/like/2")
@@ -69,3 +72,11 @@ def test_delete_a_reply():
 
 
 #Removing Post
+def test_delete_a_reply():
+    response = requests.delete("http://127.0.0.1:5000/post/delete/6")
+    assert response.status_code == 201
+# Trying to delete the already removed post
+def test_delete_a_reply():
+    response = requests.delete("http://127.0.0.1:5000/post/delete/6")
+    assert response.status_code == 400
+
